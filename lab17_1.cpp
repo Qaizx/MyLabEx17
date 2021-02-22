8a11ad0c040f6faa6470ac7c97e2736eb3f8d9d7
Qaizx <kaothom@gmail.com>
2021-02-23 00:31:53 +0700

    finish

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -7,7 +7,13 @@
 
 using namespace std;
 
-
+char score2grade(int score){
+    if(score >= 80) return 'A';
+    if(score >= 70) return 'B';
+    if(score >= 60) return 'C';
+    if(score >= 50) return 'D';
+    else return 'F';
+}
 
 string toUpperStr(string x){
     string y = x;
